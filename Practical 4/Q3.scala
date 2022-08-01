2a621d2c764da7a732814288dcee85cc42904f32
Sachini Usha <sachini.usha123@gmail.com>
2022-08-01 22:39:57 +0530

    Change Q3 in practical 4

--- Practical 4/Q3.scala
+++ Practical 4/Q3.scala
@@ -1,19 +1,25 @@
-def toUpper(n:String):String={
-    n.toUpperCase()
-}
+object names{ 
+    def toUpper(n:String):String={
+        n.toUpperCase()
+    }   
 
-def toLower(n:String):String={
-    n.toLowerCase()
-}
-def formatNames(name: String,func: String => String): String= {
-   func(name)
-}
+    def toLower(n:String):String={
+        n.toLowerCase()
+    }
+    def formatNames(name: String,func: String => String): String= {
+        func(name)
+    }
 
 
-@main
-def run():Unit={
-    println(formatNames("Benny",toUpper))
-    println("N"+(formatNames("i",toUpper))+"roshan")
-    println(formatNames("Saman",toLower))
-    println("Kumar"+(formatNames("a",toUpper))) 
+    def main(args:Array[String]):Unit={
+        val name1="Benny"
+        val name2="Nirmal"
+        val name3="Saman"
+        val name4="Kumara"
+
+        println(formatNames("Benny",toUpper));
+        println((formatNames("Niroshan".substring(0,2),toUpper))+formatNames(name2.substring(2),toLower));
+        println(formatNames("Saman",toLower));
+        println((formatNames("Kumara".substring(0,1),toUpper))+formatNames(name4.substring(1,5),toLower)+formatNames(name4.substring(5),toUpper));
+    }
 }
